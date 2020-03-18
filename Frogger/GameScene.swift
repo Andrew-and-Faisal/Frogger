0bfd666c2ee921f36f39cfaffb1db231cf4adab5
Andrew Lvovsky <alvov001@ucr.edu>
2020-03-17 22:57:47 -0700

    Tile Mapping and Frogger Character
    * created multiple tile map nodes for each area of the map
    * added unique Frogger character to the game

--- Frogger/GameScene.swift
+++ Frogger/GameScene.swift
@@ -10,80 +10,86 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+  
+  // constants
+  let waterMaxSpeed: CGFloat = 200
+  let landMaxSpeed: CGFloat = 4000
+
+  // if within threshold range of the target, frog begins slowing
+  let targetThreshold:CGFloat = 200
+
+  var maxSpeed: CGFloat = 0
+  var acceleration: CGFloat = 0
+  
+  // touch location
+  var targetLocation: CGPoint = .zero
+  
+  // Scene Nodes
+  var frog:SKSpriteNode!
+
+  override func didMove(to view: SKView) {
+    loadSceneNodes()
+    physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    maxSpeed = landMaxSpeed
+  }
+  
+  func loadSceneNodes() {
+    guard let frog = childNode(withName: "frog") as? SKSpriteNode else {
+      fatalError("Sprite Nodes not loaded")
+    }
+    self.frog = frog
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    targetLocation = touch.location(in: self)
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    targetLocation = touch.location(in: self)
+  }
+  
+  
+  override func update(_ currentTime: TimeInterval) {
+  }
+  
+  override func didSimulatePhysics() {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let offset = CGPoint(x: targetLocation.x - frog.position.x,
+                         y: targetLocation.y - frog.position.y)
+    let distance = sqrt(offset.x * offset.x + offset.y * offset.y)
+    let frogDirection = CGPoint(x:offset.x / distance,
+                               y:offset.y / distance)
+    let frogVelocity = CGPoint(x: frogDirection.x * acceleration,
+                              y: frogDirection.y * acceleration)
     
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+    frog.physicsBody?.velocity = CGVector(dx: frogVelocity.x, dy: frogVelocity.y)
+    
+    if acceleration > 5 {
+      frog.zRotation = atan2(frogVelocity.y, frogVelocity.x)
     }
     
+    // update acceleration
+    // frog speeds up to maximum
+    // if within threshold range of the target, frog begins slowing
+    // if maxSpeed has reduced due to different tiles,
+    // may need to decelerate slowly to the new maxSpeed
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    if distance < targetThreshold {
+      let delta = targetThreshold - distance
+      acceleration = acceleration * ((targetThreshold - delta)/targetThreshold)
+      if acceleration < 2 {
+        acceleration = 0
+      }
+    } else {
+      if acceleration > maxSpeed {
+        acceleration -= min(acceleration - maxSpeed, 80)
+      }
+      if acceleration < maxSpeed {
+        acceleration += min(maxSpeed - acceleration, 40)
+      }
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+
+  }
 }
